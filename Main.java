4568fa15d9c6a7ff30e5dc4d46def95a021ce96a
Ethann151 <ethan.frederik.nudelman@gmail.com>
2025-10-22 09:26:02 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("What is your name??");
+        System.out.print("What is your name? ");
         String name = scanner.nextLine();
-        
-        System.out.print("Your name is " + name + " :)");
+        System.out.println("Your name is " + name);
+        System.out.print("How old are you? ");
+        String age = scanner.nextLine();
+        System.out.println("You are " + age + " years old.");
         
         scanner.close();
         System.out.print("hello");
